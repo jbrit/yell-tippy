3ebeb12ef88b7dd522fffd3a7dc0602dcf3fe001
jbrit <pro.ajibolaojo@gmail.com>
2026-02-08 16:21:52 +0100

    the void.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: "Tippy",
+  projectId: "d89347ddc8ec2c65526a6e81cf051539",
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: true,
+});
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
